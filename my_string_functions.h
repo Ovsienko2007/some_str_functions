e1f71a9ef187182a102bea48e106ebe4c5b26bac
Ovsienko2007 <i_dont_now2@mail.ru>
2025-09-05 19:51:10 +0300

    add documentation

--- my_string_functions.h
+++ my_string_functions.h
@@ -4,28 +4,115 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//TODO documentation
-
+/**
+ * @brief reads n characters from the stream to string and put it in calloc
+ * 
+ * @param [out]  str       string
+ * @param [in]   num_chars number of symbols
+ * @param [in]   stream    stream
+ * 
+ * @return point to calloc
+ */
 char * my_getline(char *str, int num_chars, FILE *stream);
 
+/**
+ * @brief transform string to calloc
+ * 
+ * @param [in]  str string
+ * 
+ * @return point to calloc
+ */
 char * my_strdup(const char *str);
 
-char * my_fgets(char *str, int numChars, FILE *stream);
+/**
+ * @brief reads n characters from the stream to string
+ * 
+ * @param [out]  str      string
+ * @param [in]  num_chars number of symbols
+ * @param [in]  stream    stream
+ * 
+ * @return string
+ */
+char * my_fgets(char *str, int num_chars, FILE *stream);
 
+/**
+ * @brief change type from string to int
+ * 
+ * @param [in]  str string
+ * 
+ * @return number
+ */
 int my_atoi(const char * str);
 
+/**
+ * @brief put n symbols from one string to the end of another
+ * 
+ * @param [in]  str_dest line where symbols are added
+ * @param [out] str_add  string from that symbols are added
+ * @param [in]  len      number of added sympols
+ * 
+ * @return point on new string
+ */
 char * my_strncat(char * str_dest, const char * str_add, size_t len);
 
+/**
+ * @brief put one string to the end of another
+ * 
+ * @param [in]  str_dest line where symbols are added
+ * @param [out] str_add  string from that symbols are added
+ * 
+ * @return point on new string
+ */
 char * my_strcat(char * str_dest, const char * str_add);
 
+/**
+ * @brief copy n symbols from one string to another
+ * 
+ * @param [in]  new_str string where to copy
+ * @param [out] out_str copied string
+ * @param [in]  len     number of copied sympols
+ * 
+ * @return point on new string
+ */
 char * my_strncpy(char * new_str, const char * old_str, size_t len);
 
+/**
+ * @brief copy one string to another
+ * 
+ * @param [in]  new_str copied string
+ * @param [out] out_str string where to copy
+ * 
+ * @return point on new string
+ */
 char * my_strcpy(char * new_str, const char * old_str);
 
+/**
+ * @brief find size of string
+ * 
+ * @param [in] str string
+ * 
+ * @return size of string
+ */
 size_t my_strlen(const char * str);
 
+/**
+ * @brief find symbol in string
+ * 
+ * @param [in] str     string
+ * @param [in] symbol  symbol
+ * 
+ * @return character pointer
+ */
 char * my_strchr(char * str, int symbol);
 
+
+/**
+ * @brief print string
+ * 
+ * @param [in] str string
+ * 
+ * @return number > 1 if success else EOF
+ */
 int my_puts(const char* str);
 
 #endif
